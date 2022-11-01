2bdd7f2a215b44c25439cc1c5328229736b50f7f
Ugendarv <86180392+Ugendarv@users.noreply.github.com>
2022-11-01 16:41:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,8 +12,8 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '2741620'))
+API_HASH = environ.get('API_HASH', '8a3c89b5db3e85b5263cf93665072be5')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
 # Bot settings
